53f48adf6bd019dc1e13b15ed34e2fd93284d3d4
Xing <xingmarc@yeah.net>
2018-03-09 22:42:49 -0800

    Add a dfs function for a tree.

--- tree.clj
+++ tree.clj
@@ -1,2 +1,14 @@
-(defn walk [tree] (println (first tree)))
-(walk '(A (B (D) (E)) (C (F))))
+(defn walk
+  [tree]
+  (let [dfs (fn dfs [node]
+              (lazy-seq
+                (cons (first node)
+                      (when (rest node)
+                        (mapcat dfs (rest node))))))]
+    (dfs tree)))
+
+(def result
+  (walk '(A (B (D) (E)) (C (F)))))
+
+(println result)
+
